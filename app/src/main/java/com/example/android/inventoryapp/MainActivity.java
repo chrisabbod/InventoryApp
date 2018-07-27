2cee21b1e7bc8f0f9da64b2ed5f10298bd027377
Chris <chrisabbod@gmail.com>
2018-07-27 00:32:00 -0400

    <feat>: Add Save Icon Floating Action Bar Button to MainActivity

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.inventoryapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.android.inventoryapp.data.InventoryDbHelper;
 
@@ -16,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Setup FAB in MainActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         mDbHelper = new InventoryDbHelper(this);
     }
 
