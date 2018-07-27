7090e3f22d567bbf169fe31cd1bf7da206f09b98
Chris <chrisabbod@gmail.com>
2018-07-26 23:14:30 -0400

    <feat>: Add Constant Strings to InventoryContract Class

--- app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
@@ -1,5 +1,20 @@
 package com.example.android.inventoryapp.data;
 
-public final class InventoryContract {
+import android.provider.BaseColumns;
 
+public final class InventoryContract {
+    //Constructor created but not completed because it should never be used
+    private InventoryContract(){}
+
+    public static final class InventoryEntry implements BaseColumns{
+
+        public final static String TABLE_NAME = "inventory";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PRODUCT_NAME = "product name";
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+        public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public final static String COLUMN_PRODUCT_SUPPLIER_NAME = "supplier name";
+        public final static String COLUMN_PRODUCT_SUPPLIER_NUMBER = "supplier number";
+    }
 }
