3bcd13703db3eac1a8f98f88fe69dbcd7edf93fc
Chris <chrisabbod@gmail.com>
2018-08-13 22:35:44 -0400

    <feat>: Create Content URI in InventoryContract

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.inventoryapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.example.android.inventoryapp.data.InventoryContract.InventoryEntry;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +26,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void insertProduct(){
+        //Create a ContentValues object where column names are the keys,
+        //and milk product attributes are the values
+        ContentValues values = new ContentValues();
+        values.put(InventoryEntry.COLUMN_PRODUCT_NAME, "Milk");
+        values.put(InventoryEntry.COLUMN_PRODUCT_PRICE, "1.99");
+        values.put(InventoryEntry.COLUMN_PRODUCT_QUANTITY, "1");
+        values.put(InventoryEntry.COLUMN_PRODUCT_SUPPLIER_NAME, "Milk Man");
+        values.put(InventoryEntry.COLUMN_PRODUCT_SUPPLIER_NUMBER, "1234567");
+
+        Uri newUri = getContentResolver().insert(InventoryEntry.CONTENT_URI, values);
     }
 }
